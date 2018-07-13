98bf1e43cbe4866784f8e641a786492889860543
karim-c <kcis880@aucklanduni.ac.nz>
2018-07-13 16:50:20 +1200

    player movement using point gesture

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    private int movementVector = 2;
+    public float delayInterval = .15f; // seconds
+    public GameObject _playerObject;
+
+    private IEnumerator _spawnCoroutine;
+
+    void Awake()
+    {
+        _spawnCoroutine = MovePlayerWithDelay();
+
+    }
+
+    private IEnumerator MovePlayerWithDelay()
+    {
+        while (true)
+        {
+            MovePlayer();
+            yield return new WaitForSeconds(delayInterval);
+        }
+    }
+
+    public void StartPlayerMovement()
+    {
+        StartCoroutine(_spawnCoroutine);
+    }
+
+    public void StopPlayerMovement()
+    {
+        StopAllCoroutines();
+    }
+
+    private void MovePlayer()
+    {
+        Vector3 position = _playerObject.transform.position;
+        position.x = position.x - movementVector;
+        _playerObject.transform.position = position;
+    }
+
+
+    public void printMessageDeactivated()
+    {
+        print("Hello world deactivate");
+    }
+
+    public void printMessageActivated()
+    {
+        print("Hello world Active");
+    }
+}
