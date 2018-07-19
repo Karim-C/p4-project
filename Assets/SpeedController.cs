153f397c2f72db66593d3565785a7e23aea9a093
karim-c <kcis880@aucklanduni.ac.nz>
2018-07-20 10:06:14 +1200

    add option to change speed to the menu

---
+++ Assets/SpeedController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Hover.Core.Items.Types;
+using UnityEngine;
+
+public class SpeedController : MonoBehaviour {
+
+    public GameObject slider;
+    public GameObject headMountedRig;
+
+    private HoverItemDataSlider hoverItemDataSlider;
+    private SamplePlayerController samplePlayerController;
+
+    // Use this for initialization
+    void Start () {
+        hoverItemDataSlider = slider.GetComponent<HoverItemDataSlider>();
+        samplePlayerController = headMountedRig.GetComponent<SamplePlayerController>();
+        ResetPlayerMovementSpeed();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        //Debug.Log("" + hoverItemDataSlider.Value);
+        Debug.Log("" + samplePlayerController.Acceleration);
+        samplePlayerController.Acceleration = hoverItemDataSlider.Value;
+    }
+
+    public void ResetPlayerMovementSpeed()
+    {
+        hoverItemDataSlider.Value = 0.3f;
+    }
+}
